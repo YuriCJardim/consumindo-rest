f2c534605a96a05450ae3b287283e61b8532c6b4
YuriCJardim <yuricjardim@gmail.com>
2022-10-08 18:43:06 -0300

    First commit

---
+++ src/main/java/br/com/ycj/consumindorest/ConsumindorestApplication.java
@@ -0,0 +1,13 @@
+package br.com.ycj.consumindorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumindorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumindorestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ycj/consumindorest/ConsumindorestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ycj.consumindorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumindorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
